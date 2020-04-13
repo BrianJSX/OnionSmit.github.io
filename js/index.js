7ab7117853dc2349c858ff39078fb7b438239198
onionMit <minhho.technology@gmail.com>
2020-04-13 10:59:31 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+let element =  document.querySelectorAll('.contentBox');
+let imageLoad =  document.querySelector('.imageLoad');
+imageLoad.classList.add('animated', 'rotateIn','slower');
+let decriptionContent =  document.querySelector('.decriptionContent');
+decriptionContent.classList.add('animated', 'flipInY','slower');
+let dem = element.length;
+for (let index = 0; index < dem; index++) {
+    animateCheck(index);
+}
+function animateCheck(i){
+    element[i].classList.add('animated', 'fadeInRight','slower', );
+}
