8b055f95d46f56d9076fe54979b4781deff29997
onionMit <minhho.technology@gmail.com>
2020-04-14 09:27:48 -0700

    update 9:27p

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 
-let element =  document.querySelectorAll('.contentBox');
+
 let imageLoad =  document.querySelector('.imageLoad');
-imageLoad.classList.add('animated', 'rotateIn','slower');
 let decriptionContent =  document.querySelector('.decriptionContent');
 decriptionContent.classList.add('animated', 'flipInY','slower');
+imageLoad.classList.add('animated', 'rotateIn','slower');
+let x = 0;
+let element =  document.querySelectorAll('.contentBox');
+// document.getElementById("content").style.display = 'block';
 let dem = element.length;
 for (let index = 0; index < dem; index++) {
-    animateCheck(index);
+    element[index].classList.add('animated', 'fadeInRight','slower','delay-'+index+'s');
 }
-function animateCheck(i){
-    element[i].classList.add('animated', 'fadeInRight','slower','delay-3s' );
-}
+
